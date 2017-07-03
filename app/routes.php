9a8ea744fc5f34e12c7daf3988300797fcb18fff
GuillaumeR19 <g.rouge19@gmail.com>
2017-07-03 12:38:44 +0200

    new Modifs routes

--- app/routes.php
+++ app/routes.php
@@ -12,6 +12,9 @@ $w_routes = array(
 	['GET|POST', '/ownerProfil/changeRental', 'Rentals#change', 'rentals_change'],
 	['GET|POST', '/ownerProfil/deleteRental', 'Rentals#delete', 'rentals_delete'],
 
-    ['GET|POST', '/usersProfil/ShowProfileUsers', 'Users#showProfile', 'users_showprofile'],
+    ['GET|POST', '/groomProfil/showProfileGroom', 'Groom#ShowProfile', 'groom_showprofile'],
+	['GET|POST', '/groomProfil/changeProfileGroom', 'Groom#ChangeProfile', 'groom_changeprofile'],
+	['GET|POST', '/groomProfil/deleteProfileGroom', 'Groom#DeleteProfile', 'groom_deleteprofile'],
+    ['GET|POST', '/groomProfil/addServiceGroom', 'Groom#AddService', 'groom_addservice'],
 
 );

48b9725e7a8ea10121819cf9e1692739298b9b55
GuillaumeR19 <g.rouge19@gmail.com>
2017-07-03 10:42:42 +0200

    projectbdd

---
+++ groomatlantic.sql
@@ -0,0 +1,229 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Lun 03 Juillet 2017 à 10:39
+-- Version du serveur :  10.1.21-MariaDB
+-- Version de PHP :  7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `groomatlantic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `blacklist`
+--
+
+CREATE TABLE `blacklist` (
+  `id` int(11) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `ip_address` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `note` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `id_groom` int(11) NOT NULL,
+  `id_owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact_requests`
+--
+
+CREATE TABLE `contact_requests` (
+  `id` int(11) NOT NULL,
+  `groom_accept` tinyint(1) NOT NULL,
+  `owner_confirm` tinyint(1) NOT NULL,
+  `groom_confirm` tinyint(1) NOT NULL,
+  `date` datetime NOT NULL,
+  `id_groom` int(11) NOT NULL,
+  `id_owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `groom_services`
+--
+
+CREATE TABLE `groom_services` (
+  `id` int(11) NOT NULL,
+  `skills` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Contenu de la table `groom_services`
+--
+
+INSERT INTO `groom_services` (`id`, `skills`) VALUES
+(1, 'Accueil voyageurs'),
+(2, 'Départ voyageurs'),
+(3, 'Ménage'),
+(4, 'Entretien jardin'),
+(5, 'Entretien piscine'),
+(6, 'Bricolage/réparation');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rentals`
+--
+
+CREATE TABLE `rentals` (
+  `id` int(100) NOT NULL,
+  `in_area` int(11) NOT NULL,
+  `in_area_cb` tinyint(1) NOT NULL,
+  `out_area` int(11) NOT NULL,
+  `out_area_cb` tinyint(1) NOT NULL,
+  `pool` tinyint(1) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `street` varchar(100) NOT NULL,
+  `postcode` varchar(100) NOT NULL,
+  `id_owner` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `services_infos`
+--
+
+CREATE TABLE `services_infos` (
+  `id` int(11) NOT NULL,
+  `price` decimal(10,0) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `id_skill` int(11) NOT NULL,
+  `id_groom` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` int(100) NOT NULL,
+  `lastname` int(100) NOT NULL,
+  `email` int(100) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `postcode` varchar(50) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `photo` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `blacklist`
+--
+ALTER TABLE `blacklist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `contact_requests`
+--
+ALTER TABLE `contact_requests`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `groom_services`
+--
+ALTER TABLE `groom_services`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `rentals`
+--
+ALTER TABLE `rentals`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `services_infos`
+--
+ALTER TABLE `services_infos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `blacklist`
+--
+ALTER TABLE `blacklist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `contact_requests`
+--
+ALTER TABLE `contact_requests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `groom_services`
+--
+ALTER TABLE `groom_services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT pour la table `rentals`
+--
+ALTER TABLE `rentals`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `services_infos`
+--
+ALTER TABLE `services_infos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

2e6dbac91f3ad641481c483d2df922227bf7347c
clairek33 <claire.krucker@hotmail.fr>
2017-07-03 11:49:25 +0200

    Routes Rentals
    ajout des routes d'ajout/modif/suppression de locations

--- app/routes.php
+++ app/routes.php
@@ -7,11 +7,8 @@ $w_routes = array(
 	['GET|POST', '/login', 'Users#login', 'users_login'],
 	['GET', '/logout', 'Users#logout', 'users_logout'],
 
-	['GET|POST', '/blog/add', 'Blog#add', 'blog_add'],
-	['GET|POST', '/blog/list', 'Blog#listAll', 'blog_list'],
+	['GET|POST', '/ownerProfil/addRental', 'Rentals#add', 'rentals_add'],
+	['GET|POST', '/ownerProfil/changeRental', 'Rentals#change', 'rentals_change'],
+	['GET|POST', '/ownerProfil/deleteRental', 'Rentals#delete', 'rentals_delete'],
 
-
-	['GET', '/chat/', 'Tchat#tchatRead', 'chat_view'], // Visualiation des messages + formulaire
-	['GET|POST', '/chat/ajax/add', 'Tchat#tchatAjaxAdd', 'chat_add'], // Ajout d'un message via Ajax
-	['GET|POST', '/chat/ajax/list', 'Tchat#tchatAjaxList', 'chat_list'], // Récupération des messages via Ajax
 );

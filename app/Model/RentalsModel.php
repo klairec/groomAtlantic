f73e68a302424af867cbd4ea08e7ff9175e55471
clairek33 <claire.krucker@hotmail.fr>
2017-07-03 16:05:15 +0200

    Modele Rental

--- app/Model/RentalsModel.php
+++ app/Model/RentalsModel.php
@@ -4,5 +4,15 @@ namespace Model;
 
 class RentalsModel extends \W\Model\Model
 {
-	
+	public function findRentalsWithId(){
+		// Selectionne tous les champs de la table Rentals et l'ID du connecté
+		$sql = 'SELECT r.*, u.id FROM '.$this->table.' AS r INNER JOIN users AS u ON r.id_owner = u.id ORDER BY r.name ASC ';
+
+		$select = $this->dbh->prepare($sql);
+		if($select->execute()){
+			return $select->fetchAll(); // Renvoi les résultats
+		}
+
+		return false;
+	}
 }

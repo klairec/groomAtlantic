abfdf7a9ad9946bfaebbcdf513ed5408d3e5a058
clairek33 <claire.krucker@hotmail.fr>
2017-07-03 14:36:44 +0200

    Réécriture routes

--- app/routes.php
+++ app/routes.php
@@ -7,16 +7,16 @@ $w_routes = array(
 	['GET|POST', '/login', 'Users#login', 'users_login'],
 	['GET', '/logout', 'Users#logout', 'users_logout'],
 
+	['GET', 'users/Profile/showProfile', 'Users#showProfile', 'show_profile'],
+	['GET|POST', 'users/Profile/changeProfile', 'Users#changeProfile', 'change_profile'],
+	['GET|POST', 'users/Profile/deleteProfile', 'Users#deleteProfile', 'delete_profile'],
+	['GET|POST', 'users/Profile/changePassword', 'Users#changePassword', 'change_password'],
+
 	['GET', 'users/ownerProfile/showRentals', 'Rentals#showRentals', 'rentals_show'],
 	['GET|POST', 'users/ownerProfile/addRental', 'Rentals#addRental', 'rentals_add'],
 	['GET|POST', 'users/ownerProfile/changeRental', 'Rentals#changeRental', 'rentals_change'],
 	['GET|POST', 'users/ownerProfile/deleteRental', 'Rentals#deleteRental', 'rentals_delete'],
 
-    ['GET', 'users/Profile/showProfile', 'Users#showProfile', 'show_profile'],
-	['GET|POST', 'users/Profile/changeProfile', 'Users#changeProfile', 'change_profile'],
-	['GET|POST', 'users/Profile/deleteProfile', 'Users#deleteProfile', 'delete_profile'],
-	['GET|POST', 'users/Profile/changePassword', 'Users#changePassword', 'change_password'],
-    
 
     ['GET|POST', 'users/Profile/addService', 'Groom#addService', 'groom_addservice'],
 

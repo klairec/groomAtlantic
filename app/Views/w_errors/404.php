859134f4b5bf61c1ff6079335ebbf725d6f436d6
raphaelbouissr <raphaelbouissr.pro@outlook.fr>
2017-07-05 14:32:24 +0200

    404-layout modif

--- app/Views/w_errors/404.php
+++ app/Views/w_errors/404.php
@@ -1,5 +1,16 @@
-<?php $this->layout('layout', ['title' => 'Perdu ?']) ?>
+<?php $this->layout('layoutTestNico', ['title' => 'Perdu ?']) ?>
 
-<?php $this->start('main_content'); ?>
-<h1>404. Perdu ?</h1>
-<?php $this->stop('main_content'); ?>
+<?php $this->start('header'); ?>
+	
+	<section class="error_404">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<h1>Perdu ??</h1>
+					<img src="../public/assets/img/404.jpg" class="img-responsive" alt="Responsive image">
+				</div>
+			</div>
+		</div>
+	</section>
+
+<?php $this->stop('header'); ?>

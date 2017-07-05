ff427d165d1ffc301306ef9dbe8c8e60f39c6822
nicoulmn <nicolas_ulmann@yahoo.fr>
2017-07-05 16:40:38 +0200

    updt

--- app/Model/Reset_passwordModel.php
+++ app/Model/Reset_passwordModel.php
@@ -16,8 +16,39 @@ class Reset_passwordModel extends \W\Model\Model
 		$sth->execute();
 
 		return $sth->fetchall();
+
+
 	}
+
+		public function deleteToken($idUser, $token)
+	{
+		
+
+		$sql = 'DELETE FROM ' . $this->table . ' WHERE id_user = :idUser AND token = :token';
+		$sth = $this->dbh->prepare($sql);
+		$sth->bindValue(':token', $idUser);
+		$sth->bindValue(':idUser', $token);
+		$sth->execute();
+
+
+	}
+
+
+
+
+
 }
 
 
+
+
+
+
+/*
+$reponse = $bdd->prepare('DELETE FROM reset_password WHERE idUser = :idUser AND token = :token');
+		$reponse->bindValue(':token', $_GET['token'], PDO::PARAM_STR);
+		$reponse->bindValue(':idUser', $_GET['idUser'], PDO::PARAM_INT);
+		$reponse->execute();
+
+	*/
 

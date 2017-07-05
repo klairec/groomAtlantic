8f99005b7a04163c051f64d9a539f86257d1240d
GuillaumeR19 <g.rouge19@gmail.com>
2017-07-05 15:22:41 +0200

    Comments UP

--- app/Model/CommentsModel.php
+++ app/Model/CommentsModel.php
@@ -7,5 +7,15 @@ namespace Model;
  */
 class CommentsModel extends \W\Model\Model
 {
-    
- }
+    // Récupération des commentaires
+    public function showCommentById(){
+
+        $sql = 'SELECT c.*, u.* FROM '.$this->table.' AS c INNER JOIN users AS u ON c.id_groom = u.id ORDER BY date DESC';
+        $select = $this->dbh->prepare($sql);
+		if($select->execute()){
+			return $select->fetchAll(); // Renvoie les résultats
+		}
+		return false;
+    }
+}
+

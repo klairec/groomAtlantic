175c0ff2ad2b06b3e4e90483a63e6894ea22a74a
flobab <flobrobab@gmail.com>
2017-07-06 15:38:40 +0200

    update home

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -8,6 +8,15 @@
                 <?php echo $deco; ?>
                 <div class="table">
                     <div class="header-text">
+                     <div class="row">
+						<div class="col-md-12 text-center">
+							<h3 class="light white">Prenez soin de votre location saisonnière.</h3>
+							<h1 class="white typed">Choisissez le concierge qui vous convient</h1>
+							<br>
+							<br>
+							<!--<span class="typed-cursor">|</span>-->
+						</div>
+					</div>
                                   <?php 
     
                 if(!empty($errors)){// AFFICHE MESSAGES ERREURS/ SUCCES
@@ -31,23 +40,16 @@
 
 	       ?>
 	       
-
-                                
-
-    	<div class="col-sm-6 col-sm-offset-3">
+                        <div class="row">
+                            <div class="col-sm-6 col-sm-offset-3">
             <div id="imaginary_container"> 
                 <div class="input-group stylish-input-group">
-
-                	<form action="<?= $this->url('search_result') ?>">
-	                    <input type="text" class="form-control"  placeholder="Search" >
-	                    
-	                     <button data-toggle="modal" data-target="#modal1" class="btn btn-primary">Trouver mon Groom</button>
-	                </form>
-
+                    <input type="text" class="form-control"  placeholder="Search" >
+                    
+                     <button data-toggle="modal" data-target="#modal1" class="btn btn-primary">Trouver mon Groom</button>
                 </div>
             </div>
         </div>
-
                             </div>
                         </div>				
                     </div>
@@ -61,34 +63,53 @@
     <section>
     <div class="cut cut-top"></div>
 		<div class="container">
+
 			<div class="row intro-tables">
 				<div class="col-md-3">
 					<div class="intro-table intro-table-first">
-						<h4 class="white heading">Propriétaire</h4>
-						<i class="fa fa-line-chart" aria-hidden="true"></i>
+						<h4 class="white heading">Ben Adamson</h4>
+						
+						<div class="row">
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+						</div>
 
-						<h5 class="white heading small-pt">Des professionnels à votre service</h5>
-						<h5 class="white heading small-pt">Maximisez vos revenus locatifs saisonniers</h5>
+						<h5 class="white heading small-pt">Ronce les Bains</h5>
+						<button data-toggle="modal" data-target="#modal1" class="btn btn-primary">Voir Profil</button>
+					
 					</div>
 				</div>
 				<div class="col-md-3">
 					<div class="intro-table intro-table-second">
-						<h4 class="white heading">Concierge</h4>
-						<i class="fa fa-check" aria-hidden="true"></i>
-						<h5 class="white heading small-pt">Pourquoi pas vous? Inscrivez vous!</h5>
-						<div class="owl-testimonials bottom">
-							
+						<h4 class="white heading">Ben Adamson</h4>
+						
+						<div class="row">
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+							<i class="fa fa-star-o" aria-hidden="true"></i>
 						</div>
+
+						<h5 class="white heading small-pt">Ronce les Bains</h5>
+						<button data-toggle="modal" data-target="#modal1" class="btn btn-primary">Voir Profil</button>
 					</div>
 				</div>
 				<div class="col-md-3">
 					<div class="intro-table intro-table-third">
 
-						<h4 class="white heading">Charte de qualité</h4>
-						<i class="fa fa-star-o" aria-hidden="true"></i>
+						<h4 class="white heading">Ben Adamson</h4>
 						
+						<div class="row">
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+						</div>
 
-<h5 class="white heading small-pt">Des concierges qui s'engagent.</h5>
+						<h5 class="white heading small-pt">Ronce les Bains</h5>
+						<button data-toggle="modal" data-target="#modal1" class="btn btn-primary">Voir Profil</button>
 
 
 						<div class="owl-testimonials bottom">
@@ -99,66 +120,77 @@
 				<div class="col-md-3">
 					<div class="intro-table intro-table-four">
 					
-						<h4 class="white heading">Sans Engagement</h4>
-						<i class="fa fa-exchange" aria-hidden="true"></i>
-
-						<h5 class="white heading small-pt">Uniquement une mise en relation</h5>
-						<div class="owl-testimonials bottom">
-							
+						<h4 class="white heading">Ben Adamson</h4>
+						
+						<div class="row">
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+							<i class="fa fa-star-o" aria-hidden="true"></i>
+							<i class="fa fa-star-o" aria-hidden="true"></i>
 						</div>
+
+						<h5 class="white heading small-pt">Ronce les Bains</h5>
+						<button data-toggle="modal" data-target="#modal1" class="btn btn-primary">Voir Profil</button>
 					</div>
 				</div>
+				<div class="search">
+<button data-toggle="modal" data-target="#modal1" class="btn btn-blue">Trouver mon Groom</button>
+</div>
 			</div>
+	
 		</div>
+
 	</section>
-	<!--
+	
 	<section id="services" class="section section-padded">
 		<div class="container">
 			<div class="row text-center title">
-				<h2>Découvrez nos Grooms Atlantic</h2>
+				<h2>Nos Services</h2>
 			</div>
 			<div class="row services">
 				<div class="col-md-3">
 					<div class="service">
-						
-						<h4 class="heading">Justine B.</h4>
-						<p class="description">A elementum ligula lacus ac quam ultrices a scelerisque praesent vel suspendisse scelerisque a aenean hac montes.</p>
+						<div class="icon-holder">
+							<img src="<?= $this->assetUrl('img/icons/pont-ile-re.png')?>" alt="" class="icon">
+						</div>
+						<h4 class="heading">Propriétaire</h4>
+						<p class="description">Des professionnels à votre service. <br> Maximisez vos revenus locatifs saisonniers.</p>
 					</div>
 				</div>
 				<div class="col-md-3">
 					<div class="service">
 						<div class="icon-holder">
-							<img src="img/icons/guru-blue.png" alt="" class="icon">
+							<img src="<?= $this->assetUrl('img/icons/les-tours-de-la-rochelle.png')?>" alt="" class="icon">
 						</div>
-						<h4 class="heading">Yoga Pilates</h4>
-						<p class="description">A elementum ligula lacus ac quam ultrices a scelerisque praesent vel suspendisse scelerisque a aenean hac montes.</p>
+						<h4 class="heading">Concierge</h4>
+						<p class="description">Pourquoi pas vous? <br>Inscrivez vous!</p>
 					</div>
 				</div>
 				<div class="col-md-3">
 					<div class="service">
 						<div class="icon-holder">
-							<img src="img/icons/weight-blue.png" alt="" class="icon">
+							<img src="<?= $this->assetUrl('img/icons/plage-charenteMaritime.png')?>" alt="" class="icon">
 						</div>
-						<h4 class="heading">Power Training</h4>
-						<p class="description">A elementum ligula lacus ac quam ultrices a scelerisque praesent vel suspendisse scelerisque a aenean hac montes.</p>
+						<h4 class="heading">Carte de qualité</h4>
+						<p class="description">Des concierges qui s'engagent pour vous proposer les meilleurs services.</p>
 					</div>
 				</div>
 				<div class="col-md-3">
 					<div class="service">
 						<div class="icon-holder">
-							<img src="img/icons/heart-blue.png" alt="" class="icon">
+							<img src="<?= $this->assetUrl('img/icons/fort-boyard.png')?>" alt="" class="icon">
 						</div>
-						<h4 class="heading">Cardio Training</h4>
-						<p class="description">A elementum ligula lacus ac quam ultrices a scelerisque praesent vel suspendisse scelerisque a aenean hac montes.</p>
+						<h4 class="heading">Sans engagement</h4>
+						<p class="description">Nous proposons de vous mettre en relation avec des concierges de votre secteur géographique, pas d'abonnement. C'est gratuit!</p>
 					</div>
 				</div>
 			</div>
 		</div>
 		<div class="cut cut-bottom"></div>
 	</section>
-	-->
 	
-	<section id="team" class="section">
+	
+	<!--<section id="team" class="section">
 		<div class="container">
 
 			<div class="row title text-center">
@@ -168,14 +200,14 @@
 			<div class="row">
 				<div class="col-md-4">
 					<div class="team text-center">
-						<!--<div class="cover" style="background:url('img/team/team-cover1.jpg'); background-size:cover;">
+						<div class="cover" style="background:url('img/team/team-cover1.jpg'); background-size:cover;">
 
 							<div class="overlay text-center">
 								<h3 class="white">$69.00</h3>
 								<h5 class="light light-white">1 - 5 sessions / month</h5>
 							</div>
 						</div>
-						<img src="img/team/team3.jpg" alt="Team Image" class="avatar">-->
+						<img src="img/team/team3.jpg" alt="Team Image" class="avatar">
 						<div class="title">
 							<h4>Ben Adamson</h4>
 							<h5 class="muted regular">Ronce les Bains</h5>
@@ -190,13 +222,13 @@
 				</div>
 				<div class="col-md-4">
 					<div class="team text-center">
-						<!--<div class="cover" style="background:url('img/team/team-cover2.jpg'); background-size:cover;">
+						<div class="cover" style="background:url('img/team/team-cover2.jpg'); background-size:cover;">
 							<div class="overlay text-center">
 								<h3 class="white">$69.00</h3>
 								<h5 class="light light-white">1 - 5 sessions / month</h5>
 							</div>
 						</div>
-						<img src="img/team/team1.jpg" alt="Team Image" class="avatar">-->
+						<img src="img/team/team1.jpg" alt="Team Image" class="avatar">
 						<div class="title">
 							<h4>Eva Williams</h4>
 							<h5 class="muted regular">Royan</h5>
@@ -211,13 +243,13 @@
 				</div>
 				<div class="col-md-4">
 					<div class="team text-center">
-						<!--<div class="cover" style="background:url('img/team/team-cover3.jpg'); background-size:cover;">
+						<div class="cover" style="background:url('img/team/team-cover3.jpg'); background-size:cover;">
 							<div class="overlay text-center">
 								<h3 class="white">$69.00</h3>
 								<h5 class="light light-white">1 - 5 sessions / month</h5>
 							</div>
 						</div>
-						<img src="img/team/team2.jpg" alt="Team Image" class="avatar">-->
+						<img src="img/team/team2.jpg" alt="Team Image" class="avatar">
 						<div class="title">
 							<h4>John Phillips</h4>
 							<h5 class="muted regular">La Rochelle</h5>
@@ -232,7 +264,8 @@
 				</div>
 			</div>
 		</div>
-	</section>
+	</section> 
+	-->
 	
 	<section id="pricing" class="section">
 		<div class="container">
@@ -259,8 +292,8 @@
 							</ul>
 						</div>
 					</div>
-					<!--<div class="pricing">
-						<div class="box-main" data-img="img/pricing2.jpg">
+					<div class="pricing">
+						<!--<div class="box-main" data-img="img/pricing2.jpg">
 							<h4 class="white">Cardio Training</h4>
 							<h4 class="white regular light">$100.00 <span class="small-font">/ year</span></h4>
 							<a href="#" data-toggle="modal" data-target="#modal1" class="btn btn-white-fill">Sign Up Now</a>
@@ -274,8 +307,8 @@
 								<li>Free locker</li>
 								<li>Free before / after shower</li>
 							</ul>
-						</div>
-					</div>-->
+						</div>-->
+					</div>
 				</div>
 			</div>
 		</div>

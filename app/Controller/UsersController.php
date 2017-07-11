36fb8faeabe871e75b7c054574d2b3f9ab2be16c
clairek33 <claire.krucker@hotmail.fr>
2017-07-11 12:13:21 +0200

    ajout accents modif profile

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -322,11 +322,11 @@ class UsersController extends Controller
             }
 
             // on vérifie les champs insérés
-            if(!v::notEmpty()->stringType()->alpha()->length(3, 50)->validate($post['firstname'])){
+            if(!v::notEmpty()->stringType()->alpha('àâäçéèêîïôûùæœÀÄÂÇÉÈÊÎÏÔÛÙÆŒ-')->length(3, 50)->validate($post['firstname'])){
                 $errors[] = 'Le prénom doit comporter au moins 3 lettres.';
             }
 
-            if(!v::notEmpty()->stringType()->alpha()->length(3, 50)->validate($post['lastname'])){
+            if(!v::notEmpty()->stringType()->alpha('àâäçéèêîïôûùæœÀÄÂÇÉÈÊÎÏÔÛÙÆŒ-')->length(3, 50)->validate($post['lastname'])){
                 $errors[] = 'Le nom doit comporter au moins 3 lettres.';
             }
 
@@ -600,11 +600,11 @@ class UsersController extends Controller
             }
 
             // on vérifie les champs insérés
-            if(!v::notEmpty()->stringType()->alpha()->length(3, 50)->validate($post['firstname'])){
+            if(!v::notEmpty()->stringType()->alpha('àâäçéèêîïôûùæœÀÄÂÇÉÈÊÎÏÔÛÙÆŒ-')->length(3, 50)->validate($post['firstname'])){
                 $errors[] = 'Le prénom doit comporter au moins 3 lettres.';
             }
 
-            if(!v::notEmpty()->stringType()->alpha()->length(3, 50)->validate($post['lastname'])){
+            if(!v::notEmpty()->stringType()->alpha('àâäçéèêîïôûùæœÀÄÂÇÉÈÊÎÏÔÛÙÆŒ-')->length(3, 50)->validate($post['lastname'])){
                 $errors[] = 'Le nom doit comporter au moins 3 lettres.';
             }
 
